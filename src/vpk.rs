b7aed9333fa90e2b8cd1a05a5d971021e66dc066
philippe gaultier <philigaultier@gmail.com>
2017-02-15 18:18:18 +0100

    Comment unused enum values (for now) & add parse vpk object

--- src/vpk.rs
+++ src/vpk.rs
@@ -1,29 +1,32 @@
 // Valve Pak format
 
 use std::io::{Result, Error, ErrorKind};
+use std::collections::HashMap;
 use bitstream::BitStream;
 
 enum VPKFlag {
-    None = 0,
+    Object = 0,
     String = 1,
     I32 = 2,
     F32 = 3,
-    Pointer = 4,
-    WideString = 5,
-    Color = 6,
+    // Pointer = 4,
+    // WideString = 5,
+    // Color = 6,
     U64 = 7,
-    End = 8,
+    // End = 8,
     Skip = 11,
 }
 
+#[derive(PartialEq, Debug)]
 enum VPKValue {
     None,
+    Object(HashMap<String, Box<VPKValue>>),
     String(String),
     I32(i32),
     F32(f32),
-    Pointer,
-    WideString,
-    Color,
+    // Pointer,
+    // WideString,
+    // Color,
     U64(u64),
 }
 
@@ -34,7 +37,7 @@ pub struct Parser {
 }
 
 impl Parser {
-    pub fn parse_key_value(&mut self) -> Result<KeyValue> {
+    fn parse_key_value(&mut self) -> Result<KeyValue> {
         if self.bitstream.remaining_bytes() == 0 {
             return Ok((VPKValue::None, VPKValue::None));
         }
@@ -43,7 +46,7 @@ impl Parser {
 
         match flag {
             flag if flag == VPKFlag::Skip as u8 => Ok((VPKValue::None, VPKValue::None)),
-            flag if flag == VPKFlag::None as u8 => unimplemented!(),
+            flag if flag == VPKFlag::Object as u8 => self.parse_object(),
             flag if flag == VPKFlag::String as u8 => {
                 let k = VPKValue::String(self.bitstream.read_string()?);
                 let v = VPKValue::String(self.bitstream.read_string()?);
@@ -66,6 +69,30 @@ impl Parser {
             }
             _ => Err(Error::new(ErrorKind::InvalidData, format!("Unknown vpk flag {}", flag))),
         }
+    }
+
+    fn parse_entity(&mut self) {}
+
+    fn parse_object(&mut self) -> Result<KeyValue> {
+        let mut object = HashMap::new();
+        let name = VPKValue::String(self.bitstream.read_string()?);
+
+        loop {
+            let (k, v) = self.parse_key_value()?;
+            if v == VPKValue::None {
+                break;
+            }
+
+            if let VPKValue::String(k) = k {
+                object.insert(k, Box::new(v));
+            } else {
+                return Err(Error::new(ErrorKind::InvalidData,
+                                      format!("Invalid object key type: {:?}", k)));
+            }
+
+        }
+
+        Ok((name, VPKValue::Object(object)))
     }
 
     pub fn new(bytes: Vec<u8>) -> Parser {
