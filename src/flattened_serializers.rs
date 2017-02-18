7262e8233b8e215d42e07506b06f4b6f2243f48c
philippe gaultier <philigaultier@gmail.com>
2017-02-18 20:13:32 +0100

    Make progress on recurse_table WIP

--- src/flattened_serializers.rs
+++ src/flattened_serializers.rs
@@ -1,12 +1,19 @@
 use std::collections::HashMap;
 use property_serializers::{PropertySerializer, PropertySerializerTable};
-use dota::netmessages::{CSVCMsg_FlattenedSerializer, ProtoFlattenedSerializer_t};
+use dota::netmessages::{CSVCMsg_FlattenedSerializer, ProtoFlattenedSerializer_t,
+                        ProtoFlattenedSerializerField_t};
 
+macro_rules! get(
+    ($e:expr) => (match $e { Some(e) => e, None => return None })
+);
+
+#[derive(Clone)]
 pub struct DataTableProperty {
     field: DataTableField,
-    table: DataTable,
+    table: Option<DataTable>,
 }
 
+#[derive(Clone)]
 pub struct DataTableField {
     name: String,
     encoder: String,
@@ -17,10 +24,11 @@ pub struct DataTableField {
     low_value: f32,
     high_value: f32,
     version: i32,
-    serializer: PropertySerializer,
+    serializer: Option<PropertySerializer>,
     build: u32,
 }
 
+#[derive(Clone)]
 pub struct DataTable {
     pub name: String,
     pub flags: i32,
@@ -49,7 +57,7 @@ impl FlattenedSerializers {
     }
 
     pub fn recurse_table(&self, serializer: &ProtoFlattenedSerializer_t) -> DataTable {
-        let data_table = DataTable {
+        let mut data_table = DataTable {
             name: self.proto.get_symbols()[serializer.get_serializer_name_sym() as usize]
                 .to_string(),
             flags: 0,
@@ -57,8 +65,54 @@ impl FlattenedSerializers {
             properties: Vec::new(),
         };
 
-        // let props =
+        let props = self.proto.get_fields();
+
+        for index in serializer.get_fields_index() {
+            let index = *index as usize;
+            let ref field = props[index];
+
+            let encoder = if field.has_var_encoder_sym() {
+                self.proto.get_symbols()[field.get_var_encoder_sym() as usize].to_string() // TODO: bound check
+            } else {
+                String::new()
+            };
+
+            let f = DataTableField {
+                name: self.proto.get_symbols()[field.get_var_name_sym() as usize].to_string(), // TODO: bound check
+                index: -1,
+                encoder: encoder,
+                flags: field.get_encode_flags(),
+                bit_count: field.get_bit_count(),
+                low_value: field.get_low_value(),
+                high_value: field.get_high_value(),
+                kind: self.proto.get_symbols()[field.get_var_type_sym() as usize].to_string(), // TODO: bound check
+                version: field.get_field_serializer_version(),
+                serializer: None,
+                build: self.build,
+            };
+
+            let prop = DataTableProperty {
+                field: f,
+                table: self.get_data_table(field),
+            };
+
+            data_table.properties.push(prop);
+        }
 
         data_table
+    }
+
+    fn get_data_table(&self, field: &ProtoFlattenedSerializerField_t) -> Option<DataTable> {
+        let name_sym = if field.has_field_serializer_name_sym() {
+            Some(field.get_field_serializer_name_sym())
+        } else {
+            None
+        };
+
+        let ref name = self.proto.get_symbols()[field.get_field_serializer_name_sym() as usize]; // TODO: bound check
+        let ref version = field.get_field_serializer_version();
+
+        let version_to_serializer = get!(self.serializers.get(name));
+        version_to_serializer.get(version).cloned()
     }
 }
--- src/property_serializers.rs
+++ src/property_serializers.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+#[derive(Clone)]
 pub struct PropertySerializer {
     // decode
     // decode container
